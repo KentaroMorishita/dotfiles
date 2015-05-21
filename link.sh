9c1661b17b95ed95b3b9fc10dd0105ceb14cf548
Kentaro Morishita <morishita@paxcreation.com>
2015-05-21 20:09:44 +0900

    modified link.sh

--- link.sh
+++ link.sh
@@ -2,4 +2,8 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
+# NeoBundleのインストール
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc.vim ~/.vim/bundle/vimproc.vim
 
